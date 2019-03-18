798fc2d1def03e3324d3844056b9559b5b2966ae
SangEun <seun0101@gmail.com>
2019-03-18 22:51:27 +0900

    second commit - before refactoring

--- app/src/main/java/com/daou/todoapplication/AddTodoActivity.kt
+++ app/src/main/java/com/daou/todoapplication/AddTodoActivity.kt
@@ -3,6 +3,7 @@ package com.daou.todoapplication
 import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.ContentValues
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -14,26 +15,51 @@ import java.util.*
 
 class AddTodoActivity : AppCompatActivity() {
 
+    var isInsertItem = true
+    var dataId: Int = 99999
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_todo)
 
-        val date = Date()
-        val sdFormat = SimpleDateFormat("yyyy-MM-dd")
-        addDateView.text=sdFormat.format(date)
+        val intent: Intent = intent
 
-        addDateView.setOnClickListener{
+        if(intent.hasExtra("flag")
+            && (intent.getStringExtra("flag") == "update")
+            && (intent.hasExtra("dataItem")) ) {
+            val item = intent.getParcelableExtra<ParcelableDataItem>("dataItem")
+            isInsertItem = false
+            initDataByItem(item)
+        }
+        val date = Date()
+        val sdFormat = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN)
+        addDateView.text = sdFormat.format(date)
+
+        addDateView.setOnClickListener {
             val c = Calendar.getInstance()
             val year = c.get(Calendar.YEAR)
             val month = c.get(Calendar.MONTH)
             val day = c.get(Calendar.DAY_OF_MONTH)
 
-            DatePickerDialog(this,
-                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                    addDateView.text = "$year-${monthOfYear+1}-$dayOfMonth"
-                }, year, month, day).show()
+            DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                var mm = (month + 1).toString()
+                var dd = dayOfMonth.toString()
+                if(month < 10) {
+                    mm = "0$mm"
+                }
+                if(dayOfMonth < 10) {
+                    dd = "0$dd"
+                }
+                addDateView.text = "$year-$mm-$dd"
+            }, year, month, day).show()
         }
 
+    }
+
+    private fun initDataByItem(dataItem: ParcelableDataItem) {
+        dataId = dataItem.id
+        addTitleEditView.setText(dataItem.title)
+        addContentEditView.setText(dataItem.content)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -42,23 +68,24 @@ class AddTodoActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if(item?.itemId==R.id.menu_add){
+        if(item?.itemId == R.id.menu_add) {
             if(addTitleEditView.text.toString() != null && addContentEditView.text.toString() != null) {
                 val helper = DBHelper(this)
                 val db = helper.writableDatabase
-
-                val contentValues=ContentValues()
+                val contentValues = ContentValues()
                 contentValues.put("title", addTitleEditView.text.toString())
                 contentValues.put("content", addContentEditView.text.toString())
                 contentValues.put("date", addDateView.text.toString())
                 contentValues.put("completed", 0)
 
-                db.insert("tb_todo", null, contentValues)
-
+                if(isInsertItem) {
+                    db.insert("tb_todo", null, contentValues)
+                } else {
+                    db.update("tb_todo", contentValues, "_id=$dataId", null)
+                }
                 db.close()
 
                 setResult(Activity.RESULT_OK)
-
                 finish()
             }else {
                 Toast.makeText(this, "모든 데이터가 입력되지 않았습니다.", Toast.LENGTH_SHORT).show()
--- app/src/main/java/com/daou/todoapplication/MainActivity.kt
+++ app/src/main/java/com/daou/todoapplication/MainActivity.kt
@@ -5,21 +5,26 @@ import android.content.Intent
 import android.graphics.Rect
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import android.support.v4.view.ViewCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_main.view.*
+import kotlinx.android.synthetic.main.item_header.*
 import kotlinx.android.synthetic.main.item_header.view.*
+import kotlinx.android.synthetic.main.item_main.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
+
 class MainActivity : AppCompatActivity() {
 
+    //할일 목록
     var list: MutableList<ItemVO> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,108 +32,128 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         selectDB()
-        recyclerView.addItemDecoration(MyDecoration())
 
-        fab.setOnClickListener{
+        fab.setOnClickListener {
             val intent = Intent(this, AddTodoActivity::class.java)
+            intent.putExtra("flag", "insert")
             startActivityForResult(intent, 10)
         }
+
+        recyclerView.addItemDecoration(MyDecoration())
     }
 
-    private fun selectDB(){
+    private fun selectDB() {
         list = mutableListOf()
         val helper = DBHelper(this)
         val db = helper.readableDatabase
         val cursor = db.rawQuery("select * from tb_todo order by date desc", null)
 
         var preDate: Calendar? = null
-        while (cursor.moveToNext()) {
-            val dbdate=cursor.getString(3)
-            val date = SimpleDateFormat("yyyy-MM-dd").parse(dbdate)
+        while(cursor.moveToNext()) {
+            val dbdate = cursor.getString(3)
+            val date = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN).parse(dbdate)
             val currentDate = GregorianCalendar()
             currentDate.time = date
 
-            if(currentDate != preDate) {
+            if(!currentDate.equals(preDate)) {
                 val headerItem = HeaderItem(dbdate)
                 list.add(headerItem)
-                preDate=currentDate
+                preDate = currentDate
             }
 
-            val completed= cursor.getInt(4) != 0
+            val completed = cursor.getInt(4) != 0
             val dataItem = DataItem(cursor.getInt(0), cursor.getString(1), cursor.getString(2), completed)
             list.add(dataItem)
         }
 
-        Log.d("kkang","list size ${list.size}")
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = MyAdapter(list)
     }
 
+    //항목 추가 후 화면으로 돌아올 때 사용
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode==10 && resultCode== Activity.RESULT_OK){
+        if(requestCode==10 && resultCode== Activity.RESULT_OK) {
             selectDB()
         }
     }
 
     class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val headerView = view.itemHeaderView!!
+        val headerView = view.itemHeaderView
     }
 
     class DataViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val completedIconView = view.completedIconView!!
+        val mainItemLayout = view.mainItemLayout!!
         val itemTitleView = view.itemTitleView!!
         val itemContentView = view.itemContentView!!
+        val itemDeleteView = view.itemDeleteView!!
     }
 
     inner class MyAdapter(val list: MutableList<ItemVO>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        override fun getItemViewType(position: Int): Int {
+            return list.get(position).type
+        }
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            return if (viewType == ItemVO.TYPE_HEADER) {
+            if(viewType == ItemVO.TYPE_HEADER) {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                HeaderViewHolder(layoutInflater.inflate(R.layout.item_header, parent, false))
+                return HeaderViewHolder(layoutInflater.inflate(R.layout.item_header, parent, false))
             } else {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                DataViewHolder(layoutInflater.inflate(R.layout.item_main, parent, false))
+                return DataViewHolder(layoutInflater.inflate(R.layout.item_main, parent, false))
             }
         }
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val itemVO = list[position]
-            if (itemVO.type == ItemVO.TYPE_HEADER) {
-                //as .. type casting
+            val itemVO = list.get(position)
+
+            if(itemVO.type == ItemVO.TYPE_HEADER) {
                 val viewHolder = holder as HeaderViewHolder
                 val headerItem = itemVO as HeaderItem
                 viewHolder.headerView.text = headerItem.date
             } else {
                 val viewHolder = holder as DataViewHolder
                 val dataItem = itemVO as DataItem
-                viewHolder.itemTitleView.text = dataItem.title
-                viewHolder.itemContentView.text = dataItem.content
-
-                if(dataItem.completed){
+                viewHolder.itemTitleView.setText(dataItem.title)
+                viewHolder.itemContentView.setText(dataItem.content)
+                if(dataItem.completed) {
                     viewHolder.completedIconView.setImageResource(R.drawable.icon_completed)
                 }else {
                     viewHolder.completedIconView.setImageResource(R.drawable.icon)
                 }
 
-                viewHolder.completedIconView.setOnClickListener{
-                    val helper = DBHelper(this@MainActivity)//inner 라는 예약어가 클래스에 추가되어 있어야 한다.
-                    val db=helper.writableDatabase
+                viewHolder.completedIconView.setOnClickListener {
+                    val helper = DBHelper(this@MainActivity)
+                    val db = helper.writableDatabase
 
-                    if(dataItem.completed){
+                    if(dataItem.completed) {
                         db.execSQL("update tb_todo set completed=? where _id=?", arrayOf(0, dataItem.id))
                         viewHolder.completedIconView.setImageResource(R.drawable.icon)
-                    }else {
+                    } else {
                         db.execSQL("update tb_todo set completed=? where _id=?", arrayOf(1, dataItem.id))
                         viewHolder.completedIconView.setImageResource(R.drawable.icon_completed)
                     }
                     dataItem.completed = !dataItem.completed
                     db.close()
                 }
-            }
-        }
 
-        override fun getItemViewType(position: Int): Int {
-            return list[position].type
+                viewHolder.mainItemLayout.setOnClickListener {
+                    val intent = Intent(this@MainActivity, AddTodoActivity::class.java)
+                    val currentData = ParcelableDataItem(dataItem.id, dataItem.title, dataItem.content, dataItem.completed)
+                    intent.putExtra("flag", "update")
+                    intent.putExtra("dataItem", currentData)
+                    startActivityForResult(intent, 10)
+                }
+
+                viewHolder.itemDeleteView.setOnClickListener {
+                    val helper = DBHelper(this@MainActivity)
+                    val db = helper.writableDatabase
+
+                    db.execSQL("delete from tb_todo where _id =" + dataItem.id)
+                    db.close()
+                    selectDB()
+                }
+            }
         }
 
         override fun getItemCount(): Int {
@@ -136,26 +161,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    inner class MyDecoration : RecyclerView.ItemDecoration() {
+    inner class MyDecoration() : RecyclerView.ItemDecoration() {
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
             super.getItemOffsets(outRect, view, parent, state)
             val index = parent.getChildAdapterPosition(view)
-            Log.d("kkang","index $index.... list size: ${list.size}")
-            val itemVO = list[index]
-            if (itemVO.type == ItemVO.TYPE_DATA) {
+            val itemVO = list.get(index)
+            if(itemVO.type == ItemVO.TYPE_DATA) {
                 view.setBackgroundColor(0xFFFFFFFF.toInt())
                 ViewCompat.setElevation(view, 10.0f)
             }
-            outRect.set(20, 10, 20, 10)
+
+            outRect.set(30, 20, 30, 20)
         }
     }
+
 }
 
 abstract class ItemVO {
     abstract val type: Int
     companion object {
-        const val TYPE_HEADER = 0
-        const val TYPE_DATA = 1
+        val TYPE_HEADER = 0
+        val TYPE_DATA = 1
     }
 }
 
@@ -164,7 +190,41 @@ class HeaderItem(var date: String) : ItemVO() {
         get() = ItemVO.TYPE_HEADER
 }
 
-internal class DataItem(var id: Int, var title: String, var content: String, var completed: Boolean = false) : ItemVO() {
+internal class DataItem(var id: Int, var title: String, var content: String,
+                        var completed: Boolean = false) : ItemVO() {
     override val type: Int
         get() = ItemVO.TYPE_DATA
 }
+
+internal class ParcelableDataItem constructor(var id: Int, var title: String, var content: String,
+                                              var completed: Boolean = false) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readByte() != 0.toByte()
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeInt(id)
+        dest.writeString(title)
+        dest.writeString(content)
+        dest.writeValue(completed)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<ParcelableDataItem> {
+        override fun createFromParcel(parcel: Parcel): ParcelableDataItem {
+            return ParcelableDataItem(parcel)
+        }
+
+        override fun newArray(size: Int): Array<ParcelableDataItem?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
+
